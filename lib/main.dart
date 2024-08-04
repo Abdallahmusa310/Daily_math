34e340990a85fa5d9f9b2e2109b07aad81dbb6cb
AbdallahAli <Abali2792004@gmail.com>
2024-08-04 21:09:57 +0300

    change theme and create home view

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,12 @@ class Dailymath extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return  MaterialApp(
+      theme: ThemeData.dark(),
+      home: const Scaffold() ,
+    );
   }
 }
+    
+   
+     
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:daily_math/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const Dailymath());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
