f1e34d9b0d800d4dfc356886b814cdc852d63094
AbdallahAli <Abali2792004@gmail.com>
2024-08-11 03:28:34 +0300

    create adabter for model

--- lib/models/card_model.dart
+++ lib/models/card_model.dart
@@ -1,6 +1,14 @@
-class Cardmodel {
+import 'package:hive/hive.dart';
+part 'card_model.g.dart';
+
+@HiveType(typeId: 0)
+class Cardmodel extends HiveObject {
+
+  @HiveType(typeId: 0)
   final String groupname;
+  @HiveType(typeId: 1)
   final String groupgrade;
+  @HiveType(typeId: 2)
   final String grouptime;
 
 
---
+++ lib/models/card_model.g.dart
@@ -0,0 +1,40 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'card_model.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class CardmodelAdapter extends TypeAdapter<Cardmodel> {
+  @override
+  final int typeId = 0;
+
+  @override
+  Cardmodel read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return Cardmodel(
+      groupname: fields[0] as String,
+      groupgrade: fields[1]as String,
+      grouptime: fields[2] as String
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, Cardmodel obj) {
+    writer.writeByte(0);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is CardmodelAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
